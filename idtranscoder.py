14d6a1632f2d3ef49a3c25ac3b3e3eb79d76b828
Gerkano <geraldasch@gmail.com>
2022-06-07 21:49:53 +0300

    Commit these changes

--- idtranscoder.py
+++ idtranscoder.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 personal_code=str(input("iveskite koda: "))
-true_or_false=(personal_code.isdigit())
-list_of_personal_code=list(personal_code)
-o=len(personal_code)
+code_len = len(personal_code)
 count = 1
 count2 = 3
 c1 = 0
@@ -14,21 +12,21 @@ cent3 = "20"
 year1 = personal_code[1:3]
 month1 = personal_code[3:5]
 day1 = personal_code[5:7]
-a=int(list_of_personal_code[0])
-l=int(list_of_personal_code[10])
+first_num = int(personal_code[0])
+last_num = int(personal_code[10])
 
-if a == 1 or a == 2:
+if first_num == 1 or first_num == 2:
     year1 = cent1 + year1
-elif a == 3 or a == 4:
+elif first_num == 3 or first_num == 4:
     year1 = cent2 + year1
-elif a == 5 or a == 6:
+elif first_num == 5 or first_num == 6:
     year1 = cent3 + year1
 else:
     print("Wrong input")
     
-if o == 11:
+if code_len == 11:
 
-    # a=int(list_of_personal_code[0])
+    # first_num=int(list_of_personal_code[0])
     # b=int(list_of_personal_code[1])
     # c=int(list_of_personal_code[2])
     # d=int(list_of_personal_code[3])
@@ -38,26 +36,26 @@ if o == 11:
     # h=int(list_of_personal_code[7])
     # j=int(list_of_personal_code[8])
     # k=int(list_of_personal_code[9])
-    # l=int(list_of_personal_code[10])
+    # last_num=int(list_of_personal_code[10])
 
-    for number in range(o-1):
-        c1 = c1 + (int(list_of_personal_code[number]) * count)
+    for number in range(code_len-1):
+        c1 = c1 + (int(personal_code[number]) * count)
         count = count+1
         if count == 9:
             count = 1
             continue
     
-    for number in range(o-1):
-        c2 = c2 + (int(list_of_personal_code[number]) * count2)
+    for number in range(code_len-1):
+        c2 = c2 + (int(personal_code[number]) * count2)
         count2 = count2+1
         if count2 == 9:
             count2 = 1
             continue
 
     controlm1 = c1 % 11
-    # controlm1 = ((a * 1) + ( b * 2) + ( c * 3 )+ ( d * 4 )+ ( e * 5 ) + ( f * 6 ) + ( g * 7 )+ ( h * 8 ) + ( j * 9 ) + ( k * 1 )) % 11
+    # controlm1 = ((first_num * 1) + ( b * 2) + ( c * 3 )+ ( d * 4 )+ ( e * 5 ) + ( f * 6 ) + ( g * 7 )+ ( h * 8 ) + ( j * 9 ) + ( k * 1 )) % 11
     controlm2 = c2 % 11
-    # controlm2 = (( a * 3 ) + ( b * 4)  + ( c * 5) + ( d * 6) + (e * 7) + (f * 8) + (g * 9) + (h * 1) + (j * 2) + (k * 3)) % 11
+    # controlm2 = (( first_num * 3 ) + ( b * 4)  + ( c * 5) + ( d * 6) + (e * 7) + (f * 8) + (g * 9) + (h * 1) + (j * 2) + (k * 3)) % 11
 
 
     def methods():
@@ -75,7 +73,7 @@ if o == 11:
     except:
         print("invalid input")
 
-    if (z == l):
+    if (z == last_num):
         print("Personal code is valid")
     else:
         print("Personal code is not valid")
