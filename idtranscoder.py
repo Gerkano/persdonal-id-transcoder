6245a9e8006020f393b3bb4908991e3625e1471b
Gerkano <geraldasch@gmail.com>
2022-06-08 01:43:34 +0300

    Commit these changes

--- idtranscoder.py
+++ idtranscoder.py
@@ -1,81 +1,87 @@
 from datetime import datetime
 
-personal_code=str(input("iveskite koda: "))
-code_len = len(personal_code)
-count = 1
-count2 = 3
-c1 = 0
-c2 = 0
-cent1 = "18"
-cent2 = "19"
-cent3 = "20"
-year1 = personal_code[1:3]
-month1 = personal_code[3:5]
-day1 = personal_code[5:7]
-first_num = int(personal_code[0])
-last_num = int(personal_code[10])
+personal_code1=str(input("iveskite koda: "))
 
-if first_num == 1 or first_num == 2:
-    year1 = cent1 + year1
-elif first_num == 3 or first_num == 4:
-    year1 = cent2 + year1
-elif first_num == 5 or first_num == 6:
-    year1 = cent3 + year1
-else:
-    print("Wrong input")
-    
-if code_len == 11:
+class PersonalID:
 
-    # first_num=int(list_of_personal_code[0])
-    # b=int(list_of_personal_code[1])
-    # c=int(list_of_personal_code[2])
-    # d=int(list_of_personal_code[3])
-    # e=int(list_of_personal_code[4])
-    # f=int(list_of_personal_code[5])
-    # g=int(list_of_personal_code[6])
-    # h=int(list_of_personal_code[7])
-    # j=int(list_of_personal_code[8])
-    # k=int(list_of_personal_code[9])
-    # last_num=int(list_of_personal_code[10])
+    def __init__(self, personal_code: str):
+        self.personal_code = personal_code
 
-    for number in range(code_len-1):
-        c1 = c1 + (int(personal_code[number]) * count)
-        count = count+1
-        if count == 9:
-            count = 1
-            continue
-    
-    for number in range(code_len-1):
-        c2 = c2 + (int(personal_code[number]) * count2)
-        count2 = count2+1
-        if count2 == 9:
-            count2 = 1
-            continue
-
-    controlm1 = c1 % 11
-    # controlm1 = ((first_num * 1) + ( b * 2) + ( c * 3 )+ ( d * 4 )+ ( e * 5 ) + ( f * 6 ) + ( g * 7 )+ ( h * 8 ) + ( j * 9 ) + ( k * 1 )) % 11
-    controlm2 = c2 % 11
-    # controlm2 = (( first_num * 3 ) + ( b * 4)  + ( c * 5) + ( d * 6) + (e * 7) + (f * 8) + (g * 9) + (h * 1) + (j * 2) + (k * 3)) % 11
-
-
-    def methods():
-        if controlm1 == 10:
-            if controlm2 == 10:
-                return 0
+    def date_validity(self) -> bool:
+        cent1 = "18"
+        cent2 = "19"
+        cent3 = "20"
+        year1 = self.personal_code[1:3]
+        month1 = self.personal_code[3:5]
+        day1 = self.personal_code[5:7]
+        try:
+            first_num = int(self.personal_code[0])
+            if first_num == 1 or first_num == 2:
+                year1 = cent1 + year1
+            elif first_num == 3 or first_num == 4:
+                year1 = cent2 + year1
+            elif first_num == 5 or first_num == 6:
+                year1 = cent3 + year1
             else:
-                return controlm2
+                return False  
+
+            if first_num<=6 and first_num>=1:
+                try:
+                    date_validity_check = datetime(int(year1), int(month1), int(day1))
+                    print(date_validity_check)
+                    return True
+                except:
+                    return False  
+        except:
+            return False  
+
+
+    def control_number(self) -> bool:
+        code_len = len(self.personal_code)
+        count = 1
+        count2 = 3
+        num_sum1 = 0
+        num_sum2 = 0
+        
+        if code_len == 11 and self.personal_code.isdigit():
+            last_num = int(self.personal_code[10])        
+            for number in range(code_len-1):
+                num_sum1 = num_sum1 + (int(self.personal_code[number]) * count)
+                count = count+1
+                if count == 9:
+                    count = 1
+                    continue
+    
+            for number in range(code_len-1):
+                num_sum2 = num_sum2 + (int(self.personal_code[number]) * count2)
+                count2 = count2+1
+                if count2 == 9:
+                    count2 = 1
+                    continue
+
+            controlm1 = num_sum1 % 11
+            controlm2 = num_sum2 % 11
+        
+            if controlm1 == 10:
+                if controlm2 == 10:
+                    return 0 == last_num
+                else:
+                    return controlm2 == last_num
+            else:
+                return controlm1 == last_num
         else:
-            return controlm1
-    z=(methods())
+            return False
+            
 
-    try:
-        date_validity = datetime(int(year1), int(month1), int(day1))
-    except:
-        print("invalid input")
+    def conclusion(self) -> bool:
+        if self.control_number() == True and self.date_validity() == True:
+           print("Personal code is valid")
+           return True
+        else:
+            print("Personal code is either incorrect or invalid")
+            return False
 
-    if (z == last_num):
-        print("Personal code is valid")
-    else:
-        print("Personal code is not valid")
-else:
-    print(f"This: {personal_code}  identification number is in wrong format")
+
+emp1 = PersonalID(personal_code1)
+
+print(emp1.conclusion())
